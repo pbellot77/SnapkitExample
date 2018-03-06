f5faab3be9712ec4e423307b1a04ce792a369405
Patrick <patrickbellot77@gmail.com>
2018-03-06 12:30:04 -0600

    add initial commit

---
+++ Pods/Target Support Files/Pods-SnapkitExample/Pods-SnapkitExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapkitExample : NSObject
+@end
+@implementation PodsDummy_Pods_SnapkitExample
+@end
--- SnapkitExample/ViewController.swift
+++ SnapkitExample/ViewController.swift
@@ -7,19 +7,73 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+	let mainView: UIView = {
+		let view = UIView()
+		view.backgroundColor = UIColor(white: 0.6, alpha: 0.4)
+		view.layer.cornerRadius = 20
+		return view
+	}()
+	
+	let redView: UIView = {
+		let view = UIView()
+		view.backgroundColor = UIColor.red
+		view.layer.cornerRadius = 20
+		return view
+	}()
+	
+	let blueView: UIView = {
+		let view = UIView()
+		view.backgroundColor = UIColor.blue
+		view.layer.cornerRadius = 20
+		return view
+	}()
+	
+	lazy var stackView: UIStackView = {
+		// remember to add 'arrangedSubviews' to stackview
+		let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+		stackView.axis = .vertical
+		stackView.distribution = .fillEqually
+		stackView.spacing = 20
+		return stackView
+	}()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		setup()
+		setupViews()
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func setup() {
+		view.backgroundColor = .white
+	
+		navigationController?.navigationBar.prefersLargeTitles = true
+		navigationController?.navigationBar.barStyle = .blackOpaque
+		navigationController?.navigationBar.topItem?.title = "Snapkit Example"
+		navigationController?.navigationBar.barTintColor = .black
+		let attributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+		navigationController?.navigationBar.largeTitleTextAttributes = attributes
+		
 	}
-
-
+	
+	func setupViews() {
+		
+		view.addSubview(mainView)
+		mainView.addSubview(stackView)
+		
+		mainView.snp.makeConstraints { (make) in
+			make.topMargin.left.equalTo(20)
+			make.bottom.right.equalTo(-20)
+		}
+		
+		stackView.snp.makeConstraints { (make) in
+			make.topMargin.left.equalTo(20)
+			make.center.equalTo(self.mainView)
+		}
+	}
 }
 
